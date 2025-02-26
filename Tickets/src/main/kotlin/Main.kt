ed095d4b383107d76d5f347542dae3e87357bfe3
snowball3605 <weslychan9@gmail.com>
2025-02-26 15:49:28 +0800

    更新Ticket新的加載方法

--- Tickets/src/main/kotlin/Main.kt
+++ Tickets/src/main/kotlin/Main.kt
@@ -6,6 +6,7 @@ import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.build.CommandData
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.components.ActionRow
 import net.dv8tion.jda.api.interactions.components.buttons.Button
@@ -14,26 +15,36 @@ import net.dv8tion.jda.api.interactions.components.buttons.Button
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 class Tickets: PluginBase() {
     override fun onStart(jda: JDA) {
-        jda.updateCommands().addCommands(Commands.slash("tickets", "Tickets Button")).queue()
+        jda.awaitReady()
+    }
 
-        jda.addEventListener(object: ListenerAdapter() {
-            override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
-                if (event.name == "Tickets") {
-                    event.replyEmbeds(EmbedBuilder()
-                        .setTitle("Ticket office")
-                        .setDescription("如有問題請點擊以下按鈕 \nIf you have any problem, please click the button below \nご質問がある場合は、下のボタンをクリックしてください。")
-                        .build())
-                        .addActionRow(Button.success("Ticket", ":envelope_with_arrow: ")).queue()
+    override fun Commands(): List<CommandData> {
+        return listOf(
+            Commands.slash("tickets", "Tickets Button"),
+        )
+    }
+
+    override fun EventListeners(): List<ListenerAdapter> {
+        return listOf(
+            object: ListenerAdapter() {
+                override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+                    if (event.name == "Tickets") {
+                        event.replyEmbeds(EmbedBuilder()
+                            .setTitle("Ticket office")
+                            .setDescription("如有問題請點擊以下按鈕 \nIf you have any problem, please click the button below \nご質問がある場合は、下のボタンをクリックしてください。")
+                            .build())
+                            .addActionRow(Button.success("Ticket", ":envelope_with_arrow: ")).queue()
+                    }
                 }
-            }
-        }, ButtonClick())
+            }, ButtonClick()
+        )
     }
 
     class ButtonClick: ListenerAdapter() {
         override fun onButtonInteraction(event: ButtonInteractionEvent) {
             if (event.componentId == "Ticket") {
-                val guild = event.guild ?: return
-                val cat = guild.getCategoryById("1344196319178260508") ?: return
+                val guild = event.guild ?: throw Exception("No guild selected")
+                val cat = guild.getCategoryById("1344196319178260508") ?: throw Exception("No Category selected")
 
                 cat.createTextChannel("Ticket-${event.user.name}")
                     .addPermissionOverride(
