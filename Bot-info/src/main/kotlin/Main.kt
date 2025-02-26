2c2799fc908e02c32e6308ec355e5dc551a14710
snowball3605 <weslychan9@gmail.com>
2025-02-26 13:58:11 +0800

    更新Bot-info

--- Bot-info/src/main/kotlin/Main.kt
+++ Bot-info/src/main/kotlin/Main.kt
@@ -1,9 +1,13 @@
 package snow
 
+import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDA
 import net.dv8tion.jda.api.OnlineStatus
 import net.dv8tion.jda.api.OnlineStatus.ONLINE
 import net.dv8tion.jda.api.entities.Activity
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import net.dv8tion.jda.api.interactions.commands.build.Commands
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -12,5 +16,21 @@ class BotInfo: PluginBase() {
 
         jda.presence.activity = Activity.playing("huh?")
         jda.awaitReady()
+
+        jda.updateCommands().addCommands(
+            Commands.slash("botinfo", "Bot Info")
+        ).queue()
+
+        jda.addEventListener(object : ListenerAdapter() {
+            override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
+                if (event.name == "botinfo") {
+                    event.replyEmbeds(EmbedBuilder()
+                        .setTitle("OmniTech Bot")
+                        .setDescription("Name: OmniTech \nVersion: 1.0.0")
+                        .setFooter("OmniTech Bot", event.jda.selfUser.avatarUrl)
+                        .build()).queue()
+                }
+            }
+        })
     }
 }
